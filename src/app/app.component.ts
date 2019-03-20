7d11cc6823e69cdaa432d7404085f6fce5f505a0
iovavlas <iovavlas@hotmail.com>
2019-03-20 10:00:27 +0100

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,35 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   /* Component properties */
   selector: 'my-app-root',          // CSS-Selector -> HTML-Element (template) in 'index.html' 
-  //templateUrl: './app.component.html',        // Content of HTML-Element 
-  //template: '<h1 class="app"> Rendering App-root component... </h1>',  
+  templateUrl: './app.component.html',        // Content of HTML-Element 
+  /*
+  template: '<h1 class="app"> Rendering App-root component... </h1>',  
   template: `               
       <div class="app"> 
         <h1> Rendering App-root component... </h1> 
         <p> Hello... {{ title }} !</p>
-        <div>
-          <h2> - Interpolation & expressions - </h2> 
-          {{ Num1 + Num2 + '!' }} 
-          {{ isHappy ? ';)' : ':(' }}
-        </div>
-        <div>
-          <h2> - Property (One way) binding - </h2> 
-          <span> {{ title }} </span> 
-          <span [innerHTML]="title"> </span> 
-          <div>
-            <input type="text"   value=""          placeholder="Input field">
-            <input type="text"  [value]=inputText  placeholder="Input field">
-            <div> You entered: {{inputText}} </div>
-          </div>
-        </div> 
-        <div>
-          <h2> - Event binding - </h2> 
-          <input type="text"  [value]=eventInputText  
-            (input)="handleInput($event)" placeholder="Input field">
-          <div> You entered: {{eventInputText}} </div>
-          <button (click)="handleClickDelete()"> Delete </button>
-        </div> 
-      </div>`,
+      </div>`, 
+  */
   styleUrls: [ './app.component.css' ]  // CSS-file for this Component  
 })
 export class AppComponent  {
