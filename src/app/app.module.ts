9ae4c164eeecc7a896ab1bd377a632519923d9c5
iovavlas <iovavlas@hotmail.com>
2019-03-20 16:12:46 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';   // e.g. Directives and Pipes 
-//import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';   // for 2 way Binding
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, CommonModule, /*FormsModule*/ ],
+  imports:      [ BrowserModule, CommonModule, FormsModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]    
 })
